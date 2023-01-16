e9fcd21f166652746426a9bf2abe5a277767520b
Baie1 <115315059+Baie1@users.noreply.github.com>
2023-01-17 00:28:38 +0600

    first commit

---
+++ src/travels.jsx
@@ -0,0 +1,190 @@
+function travels() {
+    
+    return (
+        <div class="courses">
+			<div class="container">
+				<h2>Our All The Online Courses</h2>
+				<p class="caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat mauris non dictumst in leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<div class="courses__blocks">
+					<div class="course">
+						<a class="course__img" href="">
+							
+						</a>
+						<div class="course__content">
+							<div class="course__info">
+								<div class="course__info_item">
+									
+									<span>5.8k</span>
+								</div>
+								<div class="course__info_item">
+									
+									<span>69k</span>
+								</div>
+								<div class="course__info_item">
+									
+									<span>4.5k</span>
+								</div>
+								<div class="course__info_avatar" style="background-image: url('images/courses/coursesavatar1.svg')"></div>
+							</div>
+							<div class="course__title_and_price">
+								<h5><a href="">Development</a></h5>
+								<span>$150</span>
+							</div>
+							<div class="course__text">
+								<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Placerat mauris non dictumst in leo. Lorem ipsum dolor sit.</p>
+							</div>
+						</div>
+					</div>
+					<div class="course">
+						<a class="course__img" href="">
+							
+						</a>
+						<div class="course__content">
+							<div class="course__info">
+								<div class="course__info_item">
+								
+									<span>5.8k</span>
+								</div>
+								<div class="course__info_item">
+									
+									<span>69k</span>
+								</div>
+								<div class="course__info_item">
+									
+									<span>4.5k</span>
+								</div>
+								<div class="course__info_avatar" style="background-image: url('images/courses/coursesavatar2.svg')"></div>
+							</div>
+							<div class="course__title_and_price">
+								<h5><a href="">Android Developer</a></h5>
+								<span>$130</span>
+							</div>
+							<div class="course__text">
+								<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Placerat mauris non dictumst in leo. Lorem ipsum dolor sit.</p>
+							</div>
+						</div>
+					</div>
+					<div class="course">
+						<a class="course__img" href="">
+							
+						</a>
+						<div class="course__content">
+							<div class="course__info">
+								<div class="course__info_item">
+									
+									<span>5.8k</span>
+								</div>
+								<div class="course__info_item">
+								
+									<span>69k</span>
+								</div>
+								<div class="course__info_item">
+									
+									<span>4.5k</span>
+								</div>
+								<div class="course__info_avatar" style="background-image: url('images/courses/coursesavatar3.svg')"></div>
+							</div>
+							<div class="course__title_and_price">
+								<h5><a href="">Java Script Course</a></h5>
+								<span>$140</span>
+							</div>
+							<div class="course__text">
+								<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Placerat mauris non dictumst in leo. Lorem ipsum dolor sit.</p>
+							</div>
+						</div>
+					</div>
+					<div class="course">
+						<a class="course__img" href="">
+							
+						</a>
+						<div class="course__content">
+							<div class="course__info">
+								<div class="course__info_item">
+									
+									<span>5.8k</span>
+								</div>
+								<div class="course__info_item">
+									
+									<span>69k</span>
+								</div>
+								<div class="course__info_item">
+									
+									<span>4.5k</span>
+								</div>
+								<div class="course__info_avatar" style="background-image: url('images/courses/coursesavatar3.svg')"></div>
+							</div>
+							<div class="course__title_and_price">
+								<h5><a href="">Android Developer</a></h5>
+								<span>$120</span>
+							</div>
+							<div class="course__text">
+								<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Placerat mauris non dictumst in leo. Lorem ipsum dolor sit.</p>
+							</div>
+						</div>
+					</div>
+					<div class="course">
+						<a class="course__img" href="">
+							
+						</a>
+						<div class="course__content">
+							<div class="course__info">
+								<div class="course__info_item">
+									
+									<span>5.6k</span>
+								</div>
+								<div class="course__info_item">
+									
+									<span>69k</span>
+								</div>
+								<div class="course__info_item">
+									
+									<span>4.5k</span>
+								</div>
+								<div class="course__info_avatar" style="background-image: url('images/courses/coursesavatar4.svg')"></div>
+							</div>
+							<div class="course__title_and_price">
+								<h5><a href="">Java Script Course</a></h5>
+								<span>$120</span>
+							</div>
+							<div class="course__text">
+								<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Placerat mauris non dictumst in leo. Lorem ipsum dolor sit.</p>
+							</div>
+						</div>
+					</div>
+					<div class="course">
+						<a class="course__img" href="">
+							
+						</a>
+						<div class="course__content">
+							<div class="course__info">
+								<div class="course__info_item">
+									
+									<span>5.6k</span>
+								</div>
+								<div class="course__info_item">
+									
+									<span>69k</span>
+								</div>
+								<div class="course__info_item">
+									
+									<span>4.5k</span>
+								</div>
+								<div class="course__info_avatar" style="background-image: url('images/courses/coursesavatar5.svg')"></div>
+							</div>
+							<div class="course__title_and_price">
+								<h5><a href="">Basic Marketing</a></h5>
+								<span>$100</span>
+							</div>
+							<div class="course__text">
+								<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Placerat mauris non dictumst in leo. Lorem ipsum dolor sit.</p>
+							</div>
+						</div>
+					</div>
+					<div class="button">View All Course</div>
+				</div>
+			</div>
+		</div>
+    )
+}
+
+export default User
